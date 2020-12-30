d670edc8569d30eed70da05b8e623f331879c0b9
Edward-Kabue <ekabue0@gmail.com>
2020-12-30 11:20:06 +0300

    commit all changes

---
+++ src/components/pages/Works.js
@@ -0,0 +1,42 @@
+import React from "react";
+import Project from "../layouts/Project";
+import { projects } from "../../profile";
+
+const Works = () => {
+  return (
+    <>
+      <div data-aos="zoom-in-up" data-aos-once="true" className="third">
+        <center>
+          <div className="pp-head-line">
+            <h1 id="Projects" className="red-line pp-head">
+              Past Projects
+            </h1>
+          </div>
+        </center>
+        <div className="row">
+          {projects &&
+            projects.map((x) => (
+              <Project id={x.id} url={x.url} name={x.name} skills={x.skills} />
+            ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+/* <div className="third">
+        <center>
+          <div className="pp-head-line">
+            <h1 id="Projects" className="red-line pp-head">
+              Miscellaneous
+            </h1>
+          </div>
+        </center>
+        <div className="row">
+          {miscellaneous &&
+            miscellaneous.map((x) => (
+              <Project id={x.id} url={x.url} name={x.name} />
+            ))}
+        </div> */
+
+export default Works;
---
+++ src/profile.js
@@ -0,0 +1,144 @@
+// Change your display name on tha landing display
+const header = {
+  name: "Edward",
+};
+const background = {
+  // Options: Snow or Particle
+  type: "Particle",
+};
+// Write a para about yourself here
+// To update your image, go to './styles/images.css'
+const about = {
+  paragraph:
+    "Innovative Developer with experience i n  building and maintaining responsive websites in the industry. Proficient in WordPress, Python JavaScript; plus,modern libraries and frameworks. Passionate about usability and possessworking knowledge of Adobe Photoshop and illustrator. ",
+};
+// Edit your skill and the percentage you know about it
+// To Add a skill, copy any one below and paste it after the last comma
+const skillsBar = [
+  {
+    name: "HTML5",
+    value: 85,
+  },
+  {
+    name: "CSS3",
+    value: 85,
+  },
+  {
+    name: "Javascript & Libraries",
+    value: 80,
+  },
+  {
+    name: "Wordpress & Drupal",
+    value: 85,
+  },
+  {
+    name: "Adobe photoshop & Illustrator",
+    value: 55,
+  },
+  {
+    name: "mysql",
+    value: 85,
+  },
+  {
+    name: "Python",
+    value: 85,
+  },
+];
+// Edit your projects, its name, your skills used to make it, and the url.
+// You can omit freely anything if you dont have it
+// To Add a Project, copy any one below and paste it after the last comma and increment the id's project number
+const projects = [
+  {
+    // Add image in './styles/images.css' in #project1
+    id: "project1",
+    name: "Pantry and provisions",
+    skills: ["Wordpress,Msql"],
+    url: "https://pantryprovisions.co.ke/   ",
+  },
+  {
+    // Add image in './styles/images.css' in #project2
+    id: "project2",
+    name: "Eagle hardware",
+    skills: ["Adobe Photoshop, wordpress"],
+    url: "http://www.eaglehardware.co.ke/",
+  },
+  {
+    // Add image in './styles/images.css' in #project3
+    id: "project3",
+    name: "PPT Kenya",
+    skills: ["wordpress,msql"],
+    url: "http://pptkenya.com/",
+  },
+  {
+    // Add image in './styles/images.css' in #project4
+    id: "project4",
+    name: "Yourapps Limited",
+    skills: ["Html,css,php"],
+    url: "https://yourapps.co.ke/",
+  },
+  {
+    // Add image in './styles/images.css' in #project5
+    id: "project5",
+    name: "Siddham Beverages",
+    skills: ["Wordpress,php"],
+    url: "http://www.siddhambeverages.com/",
+  },
+  {
+    // Add image in './styles/images.css' in #project6
+    id: "project6",
+    name: "Naikart",
+    skills: ["Wordpress,Mysql"],
+    url: "https://naikart.com/?v=be7f575c3bc9",
+  },
+];
+// Edit your Miscellaneous Activities, its name and the url.
+// You can omit freely anything if you dont have it
+// To Add a Activity, copy any one below and paste it after the last comma and increment the id's Miscellaneous number
+// const miscellaneous = [
+//   {
+//     // Add image in './styles/images.css' in #misc1
+//     id: "misc1",
+//     name: "Miscellaneous 1",
+//     url: "https://github.com/kaustubhai",
+//   },
+//   {
+//     // Add image in './styles/images.css' in #misc2
+//     id: "misc2",
+//     name: "Miscellaneous 2",
+//     url: "https://github.com/kaustubhai",
+//   },
+//   {
+//     // Add image in './styles/images.css' in #misc3
+//     id: "misc3",
+//     name: "Miscellaneous 3",
+//     url: "https://github.com/kaustubhai",
+//   },
+// ];
+// Contact form text, and Jotforms link
+// To get your own jotform link, go to https://www.jotform.com/
+// If you hacve the link already, paste it in the contactUrl below
+const contact = {
+  pitch: "Developered with passion.",
+  copyright: "Edward Kabue",
+  contactUrl: "",
+};
+// Paste your respective social media links. You can omit any if you dont have it
+// Upload your resume in your drive, get the shaareable link and paste it in the resume section
+const social = {
+  github: "https://github.com",
+  facebook: "https://facebook.com",
+  linkedin: "https://linkedin.com",
+  instagram: "https://instagram.com",
+  resume: "https://novoresume.com/",
+};
+// Dont change anything here
+export {
+  header,
+  background,
+  about,
+  skillsBar,
+  projects,
+  //   miscellaneous,
+  contact,
+  social,
+};
